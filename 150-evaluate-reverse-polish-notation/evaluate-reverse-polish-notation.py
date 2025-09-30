a6cc250a6eed67568fc0db0ebfb459f3b9922820
rosho19 <rushi.singaram@gmail.com>
2025-09-30 12:35:15 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (52.04%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -5,17 +5,17 @@ class Solution:
         operand2 = 0
 
         for i in tokens:
-            if i == "+" or i == "-" or i == "*" or i == "/":
-                operand2 = int(token_stack.pop())
+            if i == "+" or i == "-" or i == "*" or i == "/": # first checks if we have an operator
+                operand2 = int(token_stack.pop()) # initializes the operands to the values in the stack
                 operand1 = int(token_stack.pop())
-                if i == "+":
-                    token_stack.append(operand1 + operand2)
+                if i == "+": # completes operation based on specific operator
+                    token_stack.append(operand1 + operand2) # pushes resulting value back onto stack
                 elif i == "-":                                
                     token_stack.append(operand1 - operand2)
                 elif i == "*":              
                     token_stack.append(operand1 * operand2)
                 elif i == "/": 
-                    token_stack.append(int(operand1 / operand2))
+                    token_stack.append(int(operand1 / operand2)) # tricky, but this truncates towards 0 both ways
             else: token_stack.append(i)
 
-        return int(token_stack.pop())
+        return int(token_stack.pop()) # final value should be the result of the expression
