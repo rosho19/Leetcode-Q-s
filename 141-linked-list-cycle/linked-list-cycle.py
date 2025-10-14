f5704982327b0c21acc4ca22c48d74c52fad95bd
Rushikesh Singarapu <rushi.singaram@gmail.com>
2025-10-14 17:17:58 -0400

    Time: 40 ms (92.19%) | Memory: 19.7 MB (86.12%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -8,20 +8,12 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fastptr = head # create two pointers, use the tortoise and hare algorithm to determine if a cycle exists
         slowptr = head
-        if slowptr and slowptr.next: # sets up base case where if index 0-2 is null, then no cycle exists. If cycle exists, null should never be found
-            slowptr = slowptr.next
-            if slowptr.next:
-                fastptr = slowptr.next
-            else: 
-                return False
-        else:
-            return False
 
-        while fastptr: # runs a while loop until fastptr is null, which means no cycle exists
-            if fastptr == slowptr: # if the ptrs meet up then cycle exists since otherwise fastptr should only be ahead of slowptr
-                return True
-            if not (fastptr.next and fastptr.next.next): # checks if the fastptr reaches null before we increment it
-                return False
+        while fastptr and fastptr.next: # runs a while loop until fastptr or fastptr.next is null, which means no cycle exists
             slowptr = slowptr.next # increments the ptrs by 1 and 2 nodes
             fastptr = fastptr.next.next
+            if fastptr == slowptr: # if the ptrs meet up then cycle exists since otherwise fastptr should only be ahead of slowptr
+                return True
+            
         return False # returns true since null was reached by fastptr
+
